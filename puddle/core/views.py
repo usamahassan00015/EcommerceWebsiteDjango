5b1f45a3280489e9e1a3234d0ed9a77c4f173b63
Usama Hassan <92426507+usamahassan00015@users.noreply.github.com>
2023-08-08 13:06:12 +0500

    git add/commit

---
+++ puddle/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from item.models import Category, Item
+
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', context={'categories': categories, 'items': items})
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
---
+++ puddle/item/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from item.models import Category, Item
+
+admin.site.register(Category)
+admin.site.register(Item)
+
+
